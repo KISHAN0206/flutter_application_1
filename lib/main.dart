81891549b3480bdb7b1a3529ebe1bf70c8e89963
KISHAN AKBARI <kishanakbari977@gmail.com>
2022-12-28 22:21:28 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:convex_bottom_bar/convex_bottom_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 import 'ui/cart/cart.dart';
 import 'ui/fav/fav.dart';
 import 'ui/home/home.dart';
@@ -21,8 +22,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+
+//For Dark mode
+
+      /*themeMode: ThemeMode.dark,
+      darkTheme: ThemeData(brightness: Brightness.dark),*/
       theme: ThemeData(
-          primarySwatch: Colors.teal,
+          //primarySwatch: Colors.red,
           elevatedButtonTheme: ElevatedButtonThemeData(
               style: ElevatedButton.styleFrom(
                   primary: Colors.green,
@@ -51,24 +57,60 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
+        // backgroundColor: Colors.black,
+        backgroundColor: Colors.black,
         title: Text('Hair With Flair'),
         shadowColor: Colors.grey,
       ),
       body: _pageNo[selectedpage],
-      bottomNavigationBar: ConvexAppBar(
-        backgroundColor: Colors.teal,
-        items: [
-          TabItem(icon: Icons.home, title: 'Home'),
-          TabItem(icon: Icons.favorite, title: 'Favorite'),
-          TabItem(icon: Icons.shopping_cart, title: 'Cart'),
-          TabItem(icon: Icons.person, title: 'Profile'),
-        ],
-        initialActiveIndex: selectedpage,
-        onTap: (int index) {
-          setState(() {
-            selectedpage = index;
-          });
-        },
+      bottomNavigationBar: Container(
+        color: Colors.black,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 5,
+            horizontal: 12,
+          ),
+          child: GNav(
+              //backgroundColor: Colors.black,
+              activeColor: Colors.white,
+              color: Colors.white,
+              tabBackgroundColor: Color.fromARGB(76, 158, 158, 158),
+              iconSize: 20,
+              tabMargin: EdgeInsets.all(1),
+              tabBorder: Border.all(color: Colors.grey, width: 1),
+              tabShadow: [BoxShadow(color: Colors.grey.withOpacity(0.5), blurRadius: 8)],
+              curve: Curves.easeInOutExpo,
+              duration: Duration(milliseconds: 100),
+              gap: 5,
+              padding: EdgeInsets.all(8),
+              tabs: [
+                GButton(
+                  icon: Icons.home,
+                  text: "home",
+                ),
+                GButton(
+                  icon: Icons.favorite,
+                  text: "Favorite",
+                ),
+                GButton(
+                  icon: Icons.shopping_cart,
+                  text: "Cart",
+                ),
+                GButton(
+                  icon: Icons.person,
+                  text: "Profile",
+                ),
+              ],
+              selectedIndex: selectedpage,
+              onTabChange: (int index) {
+                setState(
+                  () {
+                    selectedpage = index;
+                  },
+                );
+              }
+              ),
+        ),
       ),
     );
   }
